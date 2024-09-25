21612ab424c83df754ea6ebe9cad14405066184f
lopdee <ugochukwulopezubani>
2024-09-25 15:32:26 +0100

    src

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">Go to About Page</Link>
+    </div>
+  );
+};
+
+export default Home;
